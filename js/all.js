1b8455deaeb2017ccd74875f123f74cfe111eeb5
andyphl <phlai951016@gmail.com>
2022-04-20 20:27:04 +0800

    refactor: Refactor and fix eslint error

--- js/all.js
+++ js/all.js
@@ -1,57 +1,45 @@
-// TODO: 修正 ESLint 錯誤、補上分號、前輩說要改單引號 QQ
-var url="https://hexschool.github.io/js-filter-data/data.json"
-var data;
+/* global axios */
+const table = document.querySelector('.table-content');
+const url = 'https://hexschool.github.io/js-filter-data/data.json';
+let data;
 
-axios.get(url)
- .then(function(res){
-  data=res.data.filter(a=>a.作物名稱)
-  // TODO: 之後拆成 renderData 函式
-  var str=""
-  data.forEach((b,index)=>{
-    // TODO: 改成 ES6 的 Template Literals (字面字串符)
-    var content="<tr><td>" + b.作物名稱 
-    +"</td><td>" + b.市場名稱
-    +"</td><td>" + b.上價
-    +"</td><td>" + b.中價
-    +"</td><td>" + b.下價
-    +"</td><td>" + b.平均價
-    +"</td><td>" + b.交易量
-    +"</td></tr>"
-    str+=content
-  })
-  table.innerHTML=str
-})
-
-var table=document.querySelector(".table-content")
-var showData=[]
-
-var category=""
-var filter=document.querySelector(".filter")
-
-filter.addEventListener("click",filterCategory)
-
-function filterCategory(e){
-  if(e.target.nodeName=="BUTTON"){
-    category=e.target.dataset.category
-    showData=data.filter((i)=>{
-      return i.種類代碼==category
-    })
-    // TODO: 之後拆成 renderData 函式
-    var str=""
-    showData.forEach((i,index)=>{
-      var content="<tr><td>" + i.作物名稱 
-      +"</td><td>" + i.市場名稱
-      +"</td><td>" + i.上價
-      +"</td><td>" + i.中價
-      +"</td><td>" + i.下價
-      +"</td><td>" + i.平均價
-      +"</td><td>" + i.交易量
-      +"</td></tr>"
-      str+=content
-    })
-    table.innerHTML=str
-  }else{
-    return;
-  }
+function renderData(cropData) {
+  let htmlStr = '';
+  cropData.forEach((crop) => {
+    const content = `
+    <tr>
+      <td>${crop['作物名稱']}</td>
+      <td>${crop['市場名稱']}</td>
+      <td>${crop['上價']}</td>
+      <td>${crop['中價']}</td>
+      <td>${crop['下價']}</td>
+      <td>${crop['平均價']}</td>
+      <td>${crop['交易量']}</td>
+    </tr>`;
+    htmlStr += content;
+  });
+  table.innerHTML = htmlStr;
 }
 
+function getData() {
+  axios.get(url)
+    .then((res) => {
+      data = res.data.filter((a) => a.作物名稱);
+      renderData(data);
+    });
+}
+
+getData();
+
+const filter = document.querySelector('.filter');
+let category = '';
+let showData = [];
+
+function filterCategory(e) {
+  if (e.target.nodeName !== 'BUTTON') return;
+  category = e.target.dataset.category;
+  showData = data.filter((i) => i.種類代碼 === category);
+  renderData(showData);
+}
+
+filter.addEventListener('click', filterCategory);
